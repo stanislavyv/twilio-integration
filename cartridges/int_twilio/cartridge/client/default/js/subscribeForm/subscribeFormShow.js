6e99d42672cd023842828ada5067de178edeadf4
Stanislav Valkov <stanislavyv31@gmail.com>
2023-02-10 16:05:34 +0200

    Refactor client-side js files

--- cartridges/int_twilio/cartridge/client/default/js/subscribeForm/subscribeFormShow.js
+++ cartridges/int_twilio/cartridge/client/default/js/subscribeForm/subscribeFormShow.js
@@ -3,24 +3,30 @@
 /**
  * Shows subscribe to product form
  */
-$(function () {
-    const subscribeSections = $(".subscribe-product-wrapper");
+function showSubscribeForm() {
+    $(function () {
+        const $subscribeSections = $(".subscribe-product-wrapper");
 
-    $(subscribeSections).each(function (index, element) {
-        const currSubscribeSection = $(element);
+        $subscribeSections.each(function (index, element) {
+            const $currSubscribeSection = $(element);
 
-        const currShowBtn = currSubscribeSection
-            .children(".subscribe-form-show-btn")
-            .first();
-
-        currShowBtn.on("click", function () {
-            currShowBtn.remove();
-
-            const currSubscribeFormWrapper = currSubscribeSection
-                .children(".subscribe-product-form-wrapper")
+            const $currShowBtn = $currSubscribeSection
+                .children(".subscribe-form-show-btn")
                 .first();
 
-            currSubscribeFormWrapper.removeClass("d-none");
+            $currShowBtn.on("click", function () {
+                $currShowBtn.remove();
+
+                const $currSubscribeFormWrapper = $currSubscribeSection
+                    .children(".subscribe-product-form-wrapper")
+                    .first();
+
+                $currSubscribeFormWrapper.removeClass("d-none");
+            });
         });
     });
-});
+}
+
+module.exports = {
+    showSubscribeForm,
+};
--- cartridges/int_twilio/cartridge/client/default/js/subscribeForm/subscribeFormSubmit.js
+++ cartridges/int_twilio/cartridge/client/default/js/subscribeForm/subscribeFormSubmit.js
@@ -2,51 +2,57 @@
 
 const formValidation = require("base/components/formValidation");
 
-$(function () {
-    const allSubscribeForms = $(".subscribe-product-form");
+function sendSubscriptionData() {
+    $(function () {
+        const $allSubscribeForms = $(".subscribe-product-form");
 
-    // Iterate over all forms in case there are several like in a product set
-    allSubscribeForms.each(function (index, element) {
-        const currSubscribeForm = $(element);
+        // Iterate over all forms in case there are several like in a product set
+        $allSubscribeForms.each(function (index, element) {
+            const $currSubscribeForm = $(element);
 
-        // Modify submit event for each form
-        currSubscribeForm.on("submit", function (e) {
-            const form = $(this);
-            e.preventDefault();
-            const url = form.attr("action");
+            // Modify submit event for each form
+            $currSubscribeForm.on("submit", function (e) {
+                const $form = $(this);
+                e.preventDefault();
+                const url = $form.attr("action");
 
-            const notification = currSubscribeForm.next(
-                ".subscribe-product-result"
-            );
+                const $notification = $currSubscribeForm.next(
+                    ".subscribe-product-result"
+                );
 
-            form.spinner().start();
-            $.ajax({
-                url: url,
-                type: "post",
-                dataType: "json",
-                data: form.serialize(),
-                success: function (data) {
-                    form.spinner().stop();
-                    if (!data.success) {
-                        formValidation(form, data);
-                        notification.addClass("alert-danger");
-                    } else {
-                        notification.addClass("alert-success");
-                    }
+                $form.spinner().start();
+                $.ajax({
+                    url: url,
+                    type: "post",
+                    dataType: "json",
+                    data: $form.serialize(),
+                    success: function (data) {
+                        $form.spinner().stop();
+                        if (!data.success) {
+                            formValidation($form, data);
+                            $notification.addClass("alert-danger");
+                        } else {
+                            $notification.addClass("alert-success");
+                        }
 
-                    notification.text(data.notificationMessage);
-                    notification.delay(5000).fadeOut("slow");
-                },
-                error: function (e) {
-                    form.spinner().stop();
-                    notification.addClass("alert-danger");
-                    notification.text(data.notificationMessage);
+                        $notification.text(data.notificationMessage);
+                        $notification.delay(5000).fadeOut("slow");
+                    },
+                    error: function (e) {
+                        $form.spinner().stop();
+                        $notification.addClass("alert-danger");
+                        $notification.text(data.notificationMessage);
 
-                    notification.delay(5000).fadeOut("slow");
-                },
+                        $notification.delay(5000).fadeOut("slow");
+                    },
+                });
+
+                return false;
             });
-
-            return false;
         });
     });
-});
+}
+
+module.exports = {
+    sendSubscriptionData,
+};
