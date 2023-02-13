992daf7a0ecf03de8d128c9d032b5d9004dee27e
Stanislav Valkov <stanislavyv31@gmail.com>
2023-02-13 12:13:38 +0200

    Dynamically change pid input value when an attribute is changed

--- cartridges/int_twilio/cartridge/client/default/js/product/detail.js
+++ cartridges/int_twilio/cartridge/client/default/js/product/detail.js
@@ -2,18 +2,74 @@
 const base = require("base/product/detail");
 
 /**
- * Shows/ Hides add to card button and subscribe form based on product availability
- * @param {Boolean} isAvailable
+ * Changes the value of the hidden input field for product id
+ * @param {JQuery} $productContainer
+ * @param {String} productId
  */
-function productAvailabilityActionsTrigger(isAvailable) {
-    if (isAvailable) {
-        $(".product-available-price-cart").removeClass("d-none");
-        $(".subscribe-product-wrapper").addClass("d-none");
-    } else {
-        $(".product-available-price-cart").addClass("d-none");
-        $(".subscribe-product-wrapper").removeClass("d-none");
+function changePidInputValue($productContainer, productId) {
+    const $subscribeProductWrapper = $productContainer
+        .find(".subscribe-product-wrapper")
+        .first();
+
+    if (
+        $subscribeProductWrapper.length > 0 &&
+        !$subscribeProductWrapper.hasClass("d-none")
+    ) {
+        const $pidInput = $subscribeProductWrapper
+            .find(".input-product-id")
+            .first();
+        $pidInput.val(productId);
     }
 }
+
+/**
+ * Shows/ Hides add to card button and subscribe form based on product availability
+ * @param {dw.catalog.Product} product
+ * @param {JQuery} $productContainer
+ */
+function productAvailabilityActionsTrigger(product, $productContainer) {
+    if (product.readyToOrder) {
+        if (product.available) {
+            $productContainer
+                .find(".product-available-price-cart")
+                .removeClass("d-none");
+            $productContainer
+                .find(".subscribe-product-wrapper")
+                .addClass("d-none");
+        } else {
+            $productContainer
+                .find(".product-available-price-cart")
+                .addClass("d-none");
+            $productContainer
+                .find(".subscribe-product-wrapper")
+                .removeClass("d-none");
+        }
+    }
+}
+
+base.updateAttribute = function () {
+    $("body").on("product:afterAttributeSelect", function (e, response) {
+        if ($(".product-detail>.bundle-items").length) {
+            response.container.data("pid", response.data.product.id);
+            response.container
+                .find(".product-id")
+                .text(response.data.product.id);
+        } else if ($(".product-set-detail").eq(0)) {
+            response.container.data("pid", response.data.product.id);
+            response.container
+                .find(".product-id")
+                .text(response.data.product.id);
+        } else {
+            $(".product-id").text(response.data.product.id);
+            $('.product-detail:not(".bundle-item")').data(
+                "pid",
+                response.data.product.id
+            );
+        }
+
+        changePidInputValue(response.container, response.data.product.id);
+    });
+};
 
 base.updateAvailability = function () {
     $("body").on("product:updateAvailability", function (e, response) {
@@ -25,7 +81,10 @@ base.updateAvailability = function () {
             .empty()
             .html(response.message);
 
-        productAvailabilityActionsTrigger(response.product.available);
+        productAvailabilityActionsTrigger(
+            response.product,
+            response.$productContainer
+        );
 
         if ($(".global-availability").length) {
             var allAvailable = $(".product-availability")
